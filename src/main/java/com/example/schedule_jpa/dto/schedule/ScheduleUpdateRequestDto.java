2fefa66c17cf4da92518964351493848f1924b5d
gganb <gksql0801@naver.com>
2025-04-01 23:28:11 +0900

    Feat : 수정용 요청 dto - 제목,내용만 수정할 수 있도록 설계해 title, contents 필드를 가짐

---
+++ src/main/java/com/example/schedule_jpa/dto/schedule/ScheduleUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.example.schedule_jpa.dto.schedule;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleUpdateRequestDto {
+
+    private final String title;
+
+    private final String contents;
+
+    public ScheduleUpdateRequestDto(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
