10a277b7d75a76b0e404e695ceef4882373d52f0
gganb <gksql0801@naver.com>
2025-04-01 23:24:39 +0900

    Feat : 수정,조회용 응답 dto

---
+++ src/main/java/com/example/schedule_jpa/dto/ScheduleResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.schedule_jpa.dto;
+
+import com.example.schedule_jpa.entity.Schedule;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ScheduleResponseDto {
+    private final Long id;
+    private final String username;
+    private final String title;
+    private final String contents;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public ScheduleResponseDto(Long id, String username, String title, String contents, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.title = title;
+        this.contents = contents;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
