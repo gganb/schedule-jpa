6c01cf8180d5ae74230b2a8d40b61d7dccda08fd
gganb <gksql0801@naver.com>
2025-04-02 15:04:44 +0900

    Feat : User 테이블 생성 - user 테이블의 id를 외래키로 설정해 schedule 테이블과 매핑함 - id,username,email,created_at,updated_at 로 구성 - schedule 테이블의 username을 제거하고 userid를 추가해 참조관계 설정

--- schedule.sql
+++ schedule.sql
@@ -1,10 +1,21 @@
-drop table schedule
+drop table schedule;
 create table schedule
 (
     id BIGINT auto_increment primary key comment '글 id',
-    username varchar(255) not null comment '작성 유저명',
+    userid bigint not null comment '유저 고유 식별자',
     title varchar(255) not null comment '할 일 제목',
     content varchar(255) not null comment '할 일 내용',
     created_at timestamp not null comment '작성일',
-    updated_at timestamp not null comment '수정일'
+    updated_at timestamp not null comment '수정일',
+    foreign key (userid) references user (id)
 );
+
+
+create table user(
+    id BIGINT auto_increment primary key comment '유저 id',
+    username varchar(255) not null comment '유저명',
+    email varchar(255) not null comment '이메일',
+    created_at timestamp not null comment '작성일',
+    updated_at timestamp not null comment '수정일'
+
+)
