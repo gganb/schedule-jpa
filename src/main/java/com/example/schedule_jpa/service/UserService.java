cae9c7af630de63b03cd12569057791254c50e32
gganb <gksql0801@naver.com>
2025-04-02 15:17:07 +0900

    Feat : lv.2 구현 - 유저 생성 : saveUser() - 유저 전체 조회 : findAllUser() - 유저 단건 조회: findUserById() - 유저 정보 수정 : updateUser() - 유저 삭제 : deleteUser() - 수정과 삭제 메서드는 url에 입력한 이름과 db의 이름이 일치하다면 수행

---
+++ src/main/java/com/example/schedule_jpa/service/UserService.java
@@ -0,0 +1,78 @@
+package com.example.schedule_jpa.service;
+
+import com.example.schedule_jpa.dto.user.SaveUserRequestDto;
+import com.example.schedule_jpa.dto.user.UpdateUserRequestDto;
+import com.example.schedule_jpa.dto.user.UserResponseDto;
+import com.example.schedule_jpa.entity.User;
+import com.example.schedule_jpa.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserResponseDto saveUser(SaveUserRequestDto requestDto) {
+        User user = new User(requestDto.getUsername(), requestDto.getEmail());
+        User savedUser = userRepository.save(user);
+        return new UserResponseDto(
+                savedUser.getId(),
+                savedUser.getUsername(),
+                savedUser.getEmail(),
+                savedUser.getCreatedAt(),
+                savedUser.getUpdatedAt()
+        );
+    }
+
+    public List<UserResponseDto> findAllUser() {
+        List<User> findUserList = userRepository.findAll();
+        if (findUserList.isEmpty()) {
+            throw new NoSuchElementException("유저 정보가 없습니다");
+        }
+        return findUserList.stream()
+                .map(user -> new UserResponseDto(
+                        user.getId(),
+                        user.getUsername(),
+                        user.getEmail(),
+                        user.getCreatedAt(),
+                        user.getUpdatedAt()
+                )).toList();
+
+    }
+
+    public UserResponseDto findUserById(Long id) {
+        User findUser = userRepository.findUserByIdOrElseThrow(id);
+        return new UserResponseDto(
+                findUser.getId(),
+                findUser.getUsername(),
+                findUser.getEmail(),
+                findUser.getCreatedAt(),
+                findUser.getUpdatedAt());
+    }
+
+    @Transactional
+    public UserResponseDto updateUser(String username, UpdateUserRequestDto requestDto) {
+        User findUser = userRepository.findUserByUsernameOrElseThrow(username);
+        findUser.update(requestDto.getUsername(), requestDto.getEmail());
+
+        return new UserResponseDto(
+                findUser.getId(),
+                findUser.getUsername(),
+                findUser.getEmail(),
+                findUser.getCreatedAt(),
+                findUser.getUpdatedAt());
+    }
+
+    public String deleteUser(String username) {
+        User findUser = userRepository.findUserByUsernameOrElseThrow(username);
+        userRepository.delete(findUser);
+
+        return "삭제되었습니다";
+    }
+}
