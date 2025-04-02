13db922a01c552c72e82a69eabcfcef3a6af9e43
gganb <gksql0801@naver.com>
2025-04-02 18:09:25 +0900

    fix : 유저별 일정 조회 api 수정 - url 변경 : /users/{userid} - 입력 값 userid로 수정

--- src/main/java/com/example/schedule_jpa/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule_jpa/repository/ScheduleRepository.java
@@ -2,15 +2,20 @@ package com.example.schedule_jpa.repository;
 
 import com.example.schedule_jpa.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @Repository
-public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
-    default Schedule findByIdOrElseThrow(Long id){
-        return findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"해당하는 일정을 찾을 수 없습니다."));
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    default Schedule findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당하는 일정을 찾을 수 없습니다."));
     }
 
+     List<Schedule> findByUserIdOrderByUpdatedAtDesc (Long userid);
 
 }
