84069912664a119563c39a2e376a013f368210da
gganb <gksql0801@naver.com>
2025-04-02 15:25:11 +0900

    fix : 필드 수정 - user 가 분리되었기에 username필드 제거

--- src/main/java/com/example/schedule_jpa/dto/schedule/request/ScheduleSaveRequestDto.java
+++ src/main/java/com/example/schedule_jpa/dto/schedule/request/ScheduleSaveRequestDto.java
@@ -1,16 +1,12 @@
-package com.example.schedule_jpa.dto.schedule;
+package com.example.schedule_jpa.dto.schedule.request;
 
 import jakarta.validation.constraints.*;
 import lombok.Getter;
-import org.hibernate.validator.constraints.Range;
 
 @Getter
 public class ScheduleSaveRequestDto {
 
-    @NotNull(message = "이름을 입력해주세요.")
-    @Size(max = 4,message = "최대 4글자까지 입력 가능합니다.")
-    @Pattern(regexp = "^[a-zA-Z가-힣]+$",message = "한글 또는 영문만 입력 가능합니다.")
-    private final String username;
+   private final String username;
 
     @NotNull(message = "제목을 입력해주세요.")
     @Size(max = 10, message = "최대 10 글자까지 입력 가능합니다.")
