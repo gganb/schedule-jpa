9dc801bd25085ed94904c8374cef33495de51ff8
gganb <gksql0801@naver.com>
2025-04-04 13:52:26 +0900

    Fix : 커스텀 예외 코드 적용

--- src/main/java/com/example/schedule_jpa/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedule_jpa/repository/ScheduleRepository.java
@@ -2,8 +2,6 @@ package com.example.schedule_jpa.repository;
 
 import com.example.schedule_jpa.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
