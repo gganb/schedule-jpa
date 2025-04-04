b76488494bcceed58605a0a6f230b8d66b7ae7c4
gganb <gksql0801@naver.com>
2025-04-04 13:50:31 +0900

    Fix : 필드 수정 - 자세한 에러 메시지 출력을 위해 code, path 필드 추가 - builder패턴 적용

--- src/main/java/com/example/schedule_jpa/dto/ErrorResponseDto.java
+++ src/main/java/com/example/schedule_jpa/dto/ErrorResponseDto.java
@@ -1,23 +1,21 @@
 package com.example.schedule_jpa.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 public class ErrorResponseDto {
     private final int status;
     private final String error;
+    private final String code;
     private final String message;
-
+    private final String path;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime timestamp;
 
-    public ErrorResponseDto(int status, String error, String message) {
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.timestamp = LocalDateTime.now();
-    }
+
 }
