3aadc701770099cd5c909bbf86f797df9464cc10
gganb <gksql0801@naver.com>
2025-04-04 13:54:05 +0900

    Fix : 코드 수정  - 사용하지 않는 import문 제거  - title 필드에 유효성 검증 어노테이션 추가

--- src/main/java/com/example/schedule_jpa/dto/schedule/request/ScheduleUpdateRequestDto.java
+++ src/main/java/com/example/schedule_jpa/dto/schedule/request/ScheduleUpdateRequestDto.java
@@ -1,10 +1,14 @@
 package com.example.schedule_jpa.dto.schedule.request;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class ScheduleUpdateRequestDto {
 
+    @Size(max = 10, message = "최대 10 글자까지 입력 가능합니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9]+$",message = "형식이 올바르지 않습니다.")
     private final String title;
 
     private final String contents;
