7fd1961531a268a0f8d4a366477ef1235ec0ca05
gganb <gksql0801@naver.com>
2025-04-04 20:07:22 +0900

    Fix : cascade 적용 - 유저 정보를 먼저 삭제시, 해당 유저가 등록한 일정도 삭제되도록 하기 위해서 schedule 테이블에 on delete casecade 적용

--- schedule.sql
+++ schedule.sql
@@ -1,4 +1,4 @@
-
+drop table schedule;
 create table schedule
 (
     id BIGINT auto_increment primary key comment '글 id',
@@ -7,7 +7,7 @@ create table schedule
     contents varchar(255) not null comment '할 일 내용',
     created_at timestamp not null comment '작성일',
     updated_at timestamp not null comment '수정일',
-    foreign key (userid) references user (id)
+    foreign key (userid) references user (id) ON DELETE CASCADE
 );
 
 
