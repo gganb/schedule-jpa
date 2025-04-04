ebcc26e5305ec92e0b4e464e5fd6cc99e1d06f45
gganb <gksql0801@naver.com>
2025-04-04 20:12:42 +0900

    Feat : 회원 정보 확인 로직 추가 - 로그인 시 데이터베이스에 존재하는 회원인지 확인하기 위해 findByEmail(String email), findByPassword(String password)를 작성 - optional<User> 반환

--- src/main/java/com/example/schedule_jpa/repository/UserRepository.java
+++ src/main/java/com/example/schedule_jpa/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.example.schedule_jpa.repository;
 import com.example.schedule_jpa.entity.User;
 import com.example.schedule_jpa.exception.CustomException;
 import com.example.schedule_jpa.exception.ErrorCode;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,14 +16,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     default User findUserByUsernameOrElseThrow(String username) {
         return findUserByUsername(username).orElseThrow(
-                () ->  new CustomException(ErrorCode.USER_NOT_FOUND));
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
     }
 
-    default User findUserByIdOrElseThrow(Long id){
+    default User findUserByIdOrElseThrow(Long id) {
         return findById(id).orElseThrow(
                 () -> new CustomException(ErrorCode.USER_NOT_FOUND));
     }
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPassword(String password);
 }
